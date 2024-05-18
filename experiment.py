e3edbbfd0262596ceaaf8b3654c460f016083e35
Sushant Gautam <susant.gautam@gmail.com>
2024-05-18 16:22:13 +0200

    update n_arms

--- experiment.py
+++ experiment.py
@@ -35,6 +35,7 @@ best_arm = args.best_arm
 
 with open(dataset, 'rb') as f:
     pref_mat = np.loadtxt(f)    
+print("Size of preference matrix:", pref_mat.shape)
 
 
 generator_fnc = lambda i, j: np.random.binomial(n=1, p=pref_mat[int(i)][int(j)], size=1)
@@ -45,6 +46,9 @@ f_rmed = lambda k: 0.3 * (k**1.01)
 algo_list= args.models
 _d = {key+"_regrets": [] for key in algo_list}
 winner = {key+"_winner": [] for key in algo_list}
+
+n_arms_ = args.n_arms if args.n_arms > 0 else len(pref_mat)
+print("Number of arms:", n_arms_)
 
 def run_algorithm(algo_name):
     match algo_name:
@@ -57,15 +61,15 @@ def run_algorithm(algo_name):
         case "BTM":
             x = BeatTheMean(len(pref_mat), horizon,  generator_fnc,  regret_fn).algo()
         case "DOUBLER":
-            x = DOUBLER(horizon, pref_mat, regret_fn, n_arms=args.n_arms).run()
+            x = DOUBLER(horizon, pref_mat, regret_fn, n_arms=n_arms_).run()
         case "SAVAGE":
-            x = SAVAGE(horizon, pref_mat, regret_fn, n_arms=args.n_arms).run()
+            x = SAVAGE(horizon, pref_mat, regret_fn, n_arms=n_arms_).run()
         case "RUCB":
-            x = RUCB(horizon, pref_mat,  n_arms=args.n_arms).run()
+            x = RUCB(horizon, pref_mat,  n_arms=n_arms_).run()
         case "RCS":
-            x = RCS(horizon, pref_mat,  n_arms=args.n_arms).run()
+            x = RCS(horizon, pref_mat,  n_arms=n_arms_).run()
         case "THOMPSON":
-            x = THOMPSON(horizon, pref_mat, regret_fn, n_arms=args.n_arms).run()
+            x = THOMPSON(horizon, pref_mat, regret_fn, n_arms=n_arms_).run()
         case _:
             raise ValueError("Invalid algorithm name")
     print(f"{algo_name}:", x[1], end=" ")
@@ -84,14 +88,65 @@ if args.save_result:
     with open(filename+"_.json", 'w') as f: json.dump(_d, f)
     print("\nResults saved as", filename+"_.json")
 
+import seaborn as sns
 extend_array = lambda input_array: np.array(list(map(lambda arr: arr + [arr[-1]] * (max(map(len, input_array)) - len(arr)), input_array)))
 for key, val in _d.items():
-    plt.plot(np.mean(extend_array(val), axis=0), label=key.split('_')[0])
+    a= extend_array(val)
+    a[a < 0] = 0
+    plt.plot(np.mean(a, axis=0), label=key.split('_')[0])
+# for key, val in _d.items():
+#     data= extend_array(val)
+#     mean_=np.mean(data, axis=0)
+#     std_=np.std(data, axis=0)
+#     plt.plot(mean_, label=key.split('_')[0])
+#     plt.fill_between(range(len(mean_)), mean_-std_, mean_+std_, alpha=0.2)
 
+#     # use seaborn for better plots
+#     sns.relplot(data=extend_array(val), kind='line', errorbar='sd')
+# import pandas as pd
+# plt.figure(figsize=(8, 4))
+# sns.set_theme(style="whitegrid")
+# for key, val in _d.items():
+#     ss= np.array(extend_array(val))
+#     mean= np.mean(ss, axis=0)
+#     std= np.std(ss, axis=0)
+#     plt.plot(mean, label=key.split('_')[0])
+#     plt.fill_between(range(horizon), mean-std, mean+std, alpha=0.2)
+#     #log scale
+# plt.xscale('log')
+# # plt.yscale('log')
+# plt.xlabel(f'Time (log scale)')
+# plt.ylabel('Cumulative Regret (log scale)')
+# plt.tight_layout()
+# plt.legend()
+# plt.savefig(f'{filename}_.png')
+    
+
+    # sampled_indices = np.linspace(0, ss.shape[1] - 1 - 1, 77777, dtype=int)
+    # sampled_array = ss[:, sampled_indices].T
+    # sampled_array_flat = sampled_array.flatten()
+    # indices_flat = np.array([sampled_indices for _ in range(ss.shape[0])]).flatten()
+    # # create df with value and index
+    # breakpoint()
+    # df_ = pd.DataFrame.from_dict({ "value": sampled_array_flat, "index": indices_flat})
+    # sns.relplot(data=df_, x="index", y="value", kind='line', legend=False)
+
+    # create df with keyfra
+    # sampled_array = ss[0, sampled_indices].T
+    # mean_regret = np.mean(sampled_array, axis=0)
+    # std_regret = np.std(sampled_array, axis=0)
+    # plt.plot(np.std(extend_array(val), axis=0), label=key.split('_')[0])
+
+    # # breakpoint()    
+    # sns.relplot(data=sampled_array, kind='line', errorbar='sd', legend=False)
+    # plt.legend([],[], frameon=False)
+
+
+# # breakpoint()
 plt.legend()
 plt.xscale('log')
 plt.yscale('log')
-plt.xlabel('Time')
-plt.ylabel('Cumulative Regret')
+plt.xlabel('Time (log scale)')
+plt.ylabel('Cumulative Regret (log scale)')
 plt.savefig(f'{filename}_.png')
 print(f"Performance plots saved as {filename}_.png")
